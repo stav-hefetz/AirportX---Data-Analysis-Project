380013f629a1744ebb9c07316d4a4993d9911dd7
stav-hefetz <rokman1111@gmail.com>
2022-03-03 15:00:57 +0200

    Initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+select * from [DsSix_customer]
+select * from [DsSix_employee]
+select ["ProductID"], count(["ItemSize"]) from [DsSix_inventoryItem] group by ["ProductID"]
+select * From [DsSix_ItemSize]
+select * From [DsSix_Manufacturer]
+select * From [DsSix_Purchase]
+select * From [DsSix_product]
+select  * From [DsSix_PurchaseItem]
+select * From [DsSix_SalaryEmployee]
+select * From [DsSix_Sale]
+select * From [DsSix_SaleItem]
+select * From [DsSix_WageEmployee]
+
